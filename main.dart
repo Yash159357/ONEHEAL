e8f74a380c2dd6d1367024b7e72987fde345d9de
Yash159357 <83387328+Yash159357@users.noreply.github.com>
2024-04-12 12:29:30 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trial/screens/auth.dart';
-import 'package:trial/screens/tabs.dart';
+//import 'package:trial/screens/home.dart';
+//import 'package:trial/screens/tabs.dart';
+//import 'package:trial/screens/tabs.dart';
 
 ColorScheme kDarkColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 0, 146, 127),
+  seedColor: const Color.fromARGB(255, 2, 23, 80),
 );
 ColorScheme kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 47, 47, 255),
+  seedColor: const Color.fromARGB(255, 29, 68, 113),
 );
 
-
 void main() {
-  runApp(ProviderScope(
-    child: MaterialApp(
-      theme: ThemeData.light().copyWith(
-        colorScheme: kColorScheme,
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-      ),
-      themeMode: ThemeMode.light,
-      home: AuthScreen(),
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+          theme: ThemeData.light().copyWith(
+            colorScheme: kColorScheme,
+          ),
+          darkTheme: ThemeData.dark().copyWith(
+            colorScheme: kDarkColorScheme,
+          ),
+          themeMode: ThemeMode.light,
+          home: AuthenticationPage()),
     ),
-  ));
+  );
 }
