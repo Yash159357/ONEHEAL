63bb096529a4b5b29118ba36a096194ab066009d
Yash159357 <83387328+Yash159357@users.noreply.github.com>
2024-04-12 12:24:18 +0530

    Update auth.dart

--- auth.dart
+++ auth.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:trial/screens/tabs.dart';
 
-class AuthScreen extends StatefulWidget {
+class AuthenticationPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _AuthScreenState();
+    return _AuthenticationPageState();
   }
 }
 
-class _AuthScreenState extends State<AuthScreen> {
+class _AuthenticationPageState extends State<AuthenticationPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Image.asset("assets/oneHealLogo.jpeg"),
-        ]
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Image(
+              image: AssetImage("asset/images/logo2.png"),
+              height: 250,
+              width: 250,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TabsScreen()),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
